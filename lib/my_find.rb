9ec50c615fda2221aea2066aa6dfdd7938407533
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-01-28 14:34:09 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_find(collection)
   i = 0 
   while i < collection.length 
-  return collection[i] if yields(collection[i])
-  i += 1 
+   return collection[i] if yields(collection[i])
+   i += 1 
 end
 end
